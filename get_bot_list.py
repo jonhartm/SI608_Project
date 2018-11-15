c268c7e58274662eb65ec8af339d8d2bc89b264a
jonhartm <jonhartm@umich.edu>
2018-11-15 14:23:25 -0500

    Fix so bot list only contains users, not subreddits

--- get_bot_list.py
+++ get_bot_list.py
@@ -9,19 +9,20 @@ page = cache.CheckCache_Soup(url)
 
 users = []
 for link in page.find("table").find_all("a"):
-    users.append(link.getText())
+    if link.getText()[1] == 'u':
+        users.append(link.getText())
 
 with open("bot_list.txt", 'w') as f:
     for u in users:
         f.write(u+'\n')
 
 # get the banned user list
-page = BeautifulSoup(open('suspiciousaccounts.html').read(), "html.parser")
-
-users = []
-for link in page.find("table").find_all("a"):
-    users.append(link.getText())
-
-with open("banned_user_list.txt", 'w') as f:
-    for u in users:
-        f.write(u+'\n')
+# page = BeautifulSoup(open('suspiciousaccounts.html').read(), "html.parser")
+#
+# users = []
+# for link in page.find("table").find_all("a"):
+#     users.append(link.getText())
+#
+# with open("banned_user_list.txt", 'w') as f:
+#     for u in users:
+#         f.write(u+'\n')
